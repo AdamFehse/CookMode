0accc604d356d4c2293ba109fe6aa5042b8a01b0
Adam Fehse <adamfehse@arizona.edu>
2025-10-09 15:15:50 -0700

    bump cache v4, DB V3

--- idb.js
+++ idb.js
@@ -1,7 +1,7 @@
 
 // --- IndexedDB constants and openDB helper ---
 const DB_NAME = 'recipecard-db';
-const DB_VERSION = 1;
+const DB_VERSION = 3;
 const STORE_DISH = 'dish';
 const STORE_PLAN = 'plan';
 const STORE_SETTINGS = 'settings';
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version on every deploy to force update
-const CACHE_NAME = 'recipecard-static-v3';
+const CACHE_NAME = 'recipecard-static-v5';
 
 const urlsToCache = [
   './',
